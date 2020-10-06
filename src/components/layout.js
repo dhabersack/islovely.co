32495830e84e18e6883e2112cff46e3ffb506040
Dom Habersack <dom@islovely.co>
2020-10-06 20:07:05 +0200

    Serve Inter and Source Code Pro from same environment
    If you don’t load these from a third party, you don’t need to block that
    third party before cookie consent was given.
    
    Galaxy brain time.

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,10 +14,7 @@ export default ({
   children
 }) => (
   <Application>
-    <Helmet htmlAttributes={IS_IN_DEBUG_MODE ? { 'class': 'debug' } : {}}>
-      <link rel="stylesheet" href="https://use.typekit.net/gbp8hqr.css" />
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Helmet>
+    <Helmet htmlAttributes={IS_IN_DEBUG_MODE ? { 'class': 'debug' } : {}} />
 
     <div className="flex flex-column height-full">
       <div className="flex-noshrink margin-horizontal-m">
