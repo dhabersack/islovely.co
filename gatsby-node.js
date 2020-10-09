7b59b1ea91a3e5e1eb41af5d7434d1daa0b43068
Dom Habersack <dom@islovely.co>
2020-10-09 11:24:29 +0200

    Move page-creation to helper function as well
    There was more unnecessary duplication here. This is crazy.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,136 +2,66 @@ const path = require(`path`)
 
 const slugify = text => text.toLowerCase().replace(/ /g, '-')
 
-const getQueryForSourceInstanceName = sourceInstanceName => `
-  {
-    allFile(filter: {
-      sourceInstanceName: {
-        eq: "${sourceInstanceName}"
-      }
-    }) {
-      edges {
-        node {
-          childMarkdownRemark {
-            fields {
-              permalink
-              slug
+const buildCreatePages = (createPage, graphql) => {
+  const TEMPLATE_PATH_BY_SOURCE_INSTANCE_NAME = {
+    courses: `src/templates/course.js`,
+    newsletters: `src/templates/newsletter.js`,
+    pages: `src/templates/page.js`,
+    posts: `src/templates/post.js`,
+    projects: `src/templates/project.js`,
+  }
+
+  return async sourceInstanceName => {
+    const response = await graphql(`
+      {
+        allFile(filter: {
+          sourceInstanceName: {
+            eq: "${sourceInstanceName}"
+          }
+        }) {
+          edges {
+            node {
+              childMarkdownRemark {
+                fields {
+                  permalink
+                  slug
+                }
+              }
             }
           }
         }
       }
+    `)
+
+    if (response.errors) {
+      reporter.panicOnBuild(`Error while running GraphQL query for “${sourceInstanceName}”.`)
+      return
     }
+
+    response.data.allFile.edges.forEach(({ node }) => {
+      const { permalink, slug } = node.childMarkdownRemark.fields
+
+      createPage({
+        component: path.resolve(TEMPLATE_PATH_BY_SOURCE_INSTANCE_NAME[sourceInstanceName]),
+        context: {
+          slug
+        },
+        path: permalink
+      })
+
+      console.log(`created page at ${permalink}`)
+    })
   }
-`
+}
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  const courses = await graphql(getQueryForSourceInstanceName('courses'))
+  const createPages = buildCreatePages(actions.createPage, graphql)
 
-  if (courses.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-
-  courses.data.allFile.edges.forEach(({ node }) => {
-    const { permalink, slug } = node.childMarkdownRemark.fields
-
-    actions.createPage({
-      component: path.resolve(`src/templates/course.js`),
-      context: {
-        slug
-      },
-      path: permalink
-    })
-
-    console.log(`created page at ${permalink}`)
-  })
-
-
-  const newsletters = await graphql(getQueryForSourceInstanceName('newsletters'))
-
-  if (newsletters.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-
-  newsletters.data.allFile.edges.forEach(({ node }) => {
-    const { permalink, slug } = node.childMarkdownRemark.fields
-
-    actions.createPage({
-      component: path.resolve(`src/templates/newsletter.js`),
-      context: {
-        slug
-      },
-      path: permalink
-    })
-
-    console.log(`created page at ${permalink}`)
-  })
-
-  const pages = await graphql(getQueryForSourceInstanceName('pages'))
-
-  if (pages.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-
-  pages.data.allFile.edges.forEach(({ node }) => {
-    const { permalink, slug } = node.childMarkdownRemark.fields
-
-    actions.createPage({
-      component: path.resolve(`src/templates/page.js`),
-      context: {
-        slug
-      },
-      path: permalink
-    })
-
-    console.log(`created page at ${permalink}`)
-  })
-
-
-  const projects = await graphql(getQueryForSourceInstanceName('projects'))
-
-  if (projects.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-
-  projects.data.allFile.edges.forEach(({ node }) => {
-    const { permalink, slug } = node.childMarkdownRemark.fields
-
-    actions.createPage({
-      component: path.resolve(`src/templates/project.js`),
-      context: {
-        slug
-      },
-      path: permalink
-    })
-
-    console.log(`created page at ${permalink}`)
-  })
-
-
-  const posts = await graphql(getQueryForSourceInstanceName('posts'))
-
-  if (posts.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-
-  posts.data.allFile.edges.forEach(({ node }) => {
-    const { permalink, slug } = node.childMarkdownRemark.fields
-
-    actions.createPage({
-      component: path.resolve(`src/templates/post.js`),
-      context: {
-        slug
-      },
-      path: permalink
-    })
-
-    console.log(`created page at ${permalink}`)
-  })
-
+  createPages('courses')
+  createPages('newsletters')
+  createPages('pages')
+  createPages('posts')
+  createPages('projects')
 
   const postsForCategories = await graphql(`
     {
