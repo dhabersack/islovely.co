84f5a70283cc9e8eb8cbe90e3aa6cf90fccc4c84
Dom Habersack <dom@islovely.co>
2020-10-09 15:52:27 +0200

    Move functions around, add temporary variables for category-generation
    The generation of the category-pages was messy before. Adding a few
    well-named variables along the way made that section more readable.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,74 +2,77 @@ const path = require(`path`)
 
 const slugify = text => text.toLowerCase().replace(/ /g, '-')
 
-const buildCreatePages = (createPage, graphql) => {
-  const TEMPLATE_PATH_BY_SOURCE_INSTANCE_NAME = {
-    courses: `src/templates/course.js`,
-    newsletters: `src/templates/newsletter.js`,
-    pages: `src/templates/page.js`,
-    posts: `src/templates/post.js`,
-    projects: `src/templates/project.js`,
-  }
+const buildCreateNodeFields = (node, createNodeField) => fields => {
+  Object.entries(fields).forEach(([name, value]) => {
+    createNodeField({
+      name,
+      node,
+      value,
+    })
+  })
+}
 
-  return async sourceInstanceName => {
-    const response = await graphql(`
-      {
-        allFile(filter: {
-          sourceInstanceName: {
-            eq: "${sourceInstanceName}"
-          }
-        }) {
-          edges {
-            node {
-              childMarkdownRemark {
-                fields {
-                  permalink
-                  slug
-                }
+const buildCreatePages = (createPage, graphql) => async sourceInstanceName => {
+  const response = await graphql(`
+    {
+      allFile(filter: {
+        sourceInstanceName: {
+          eq: "${sourceInstanceName}"
+        }
+      }) {
+        edges {
+          node {
+            childMarkdownRemark {
+              fields {
+                permalink
+                slug
+                type
               }
             }
           }
         }
       }
-    `)
-
-    if (response.errors) {
-      reporter.panicOnBuild(`Error while running GraphQL query for “${sourceInstanceName}”.`)
-      return
     }
+  `)
 
-    response.data.allFile.edges.forEach(({ node }) => {
-      const { permalink, slug } = node.childMarkdownRemark.fields
-
-      createPage({
-        component: path.resolve(TEMPLATE_PATH_BY_SOURCE_INSTANCE_NAME[sourceInstanceName]),
-        context: {
-          slug
-        },
-        path: permalink
-      })
-
-      console.log(`created page at ${permalink}`)
-    })
+  if (response.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query for “${sourceInstanceName}”.`)
+    return
   }
+
+  response.data.allFile.edges.forEach(({ node }) => {
+    const { permalink, slug, type } = node.childMarkdownRemark.fields
+
+    createPage({
+      component: path.resolve(`src/templates/${type}.js`),
+      context: {
+        slug
+      },
+      path: permalink
+    })
+
+    console.log(`created page at ${permalink}`)
+  })
 }
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const createPages = buildCreatePages(actions.createPage, graphql)
 
-  createPages('courses')
-  createPages('newsletters')
-  createPages('pages')
-  createPages('posts')
-  createPages('projects')
+  await createPages('courses')
+  await createPages('newsletters')
+  await createPages('pages')
+  await createPages('posts')
+  await createPages('projects')
 
-  const postsForCategories = await graphql(`
+  const posts = await graphql(`
     {
-      allFile(filter: {
-        sourceInstanceName: {
-          eq: "posts"
+      allFile(
+        filter: {
+          sourceInstanceName: {
+            eq: "posts"
+          }
         }
-      }) {
+      ) {
         edges {
           node {
             childMarkdownRemark {
@@ -83,8 +86,11 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     }
   `)
 
-  const allCategories = postsForCategories.data.allFile.edges.map(({ node }) => node.childMarkdownRemark.frontmatter.categories).reduce((allCategories, categories) => [...new Set([...allCategories, ...categories])], [])
-  allCategories.forEach(category => {
+  const getCategoriesForEdge = ({ node }) => node.childMarkdownRemark.frontmatter.categories
+  const allCategories = posts.data.allFile.edges.map(getCategoriesForEdge).flat()
+  const uniqueCategories = [...new Set(allCategories)]
+
+  uniqueCategories.forEach(category => {
     const permalink = `/categories/${slugify(category)}`
 
     actions.createPage({
@@ -96,16 +102,6 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     })
 
     console.log(`created page at ${permalink}`)
-  })
-}
-
-const buildCreateNodeFields = (node, createNodeField) => fields => {
-  Object.entries(fields).forEach(([name, value]) => {
-    createNodeField({
-      node,
-      name,
-      value,
-    })
   })
 }
 
