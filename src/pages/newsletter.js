a3b3f5387daa736338eb1fc06ede15a13fdebc3c
Dom Habersack <dom@islovely.co>
2020-11-03 16:18:31 +0100

    Replace icon in newsletter signup list, format code
    The old icons were no longer cool.

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -43,14 +43,23 @@ export default ({ data, location }) => {
           In addition to what I share on this site, I send out a weekly newsletter with tips to help you work smarter. By signing up, you get access to:
         </p>
 
-        <ul className="list-style-none margin-bottom-m padding-0 xs:columns-10 xs:margin-horizontal-auto m:columns-8-of-10 l:columns-6-of-8">
+        <ul
+          className="list-style-none margin-bottom-m padding-0 xs:columns-10 xs:margin-horizontal-auto m:columns-8-of-10 l:columns-6-of-8"
+        >
           {listItems.map(listItem => (
-            <li className="flex margin-bottom-xs" key={`newsletter-benefit-${listItem}`}>
-              <div className="align-items-center border-color-blue-500 border-radius-round border-style-solid border-width-m flex-no-shrink height-24 inline-flex justify-center margin-right-xxs width-24 m:height-27 m:width-27">
-                <img alt="" className="width-12 m:width-15" src="/assets/icons/checkmark--filled.svg" />
-              </div>
+            <li
+              className="flex margin-bottom-s"
+              key={`newsletter-benefit-${listItem}`}
+            >
+              <img
+                alt=""
+                className="flex-no-shrink height-24 margin-right-xs width-24 m:height-24 m:width-27"
+                src="/assets/icons/checkmark.svg"
+              />
 
-              <span className="font-size-16-medium m:font-size-18-medium">
+              <span
+                className="font-size-16-medium m:font-size-18-medium"
+              >
                 {listItem}
               </span>
             </li>
@@ -62,20 +71,36 @@ export default ({ data, location }) => {
         </p>
 
         <div className="margin-bottom-l">
-          <ConvertkitForm svForm="1067424" uid="627637e2b6" sourceUrl={location.href} />
+          <ConvertkitForm
+            sourceUrl={location.href}
+            svForm="1067424"
+            uid="627637e2b6"
+          />
         </div>
 
-        <h2>Featured newsletters</h2>
+        <h2>
+          Featured newsletters
+        </h2>
 
-        <div className="margin-bottom-l">
+        <div
+          className="margin-bottom-l"
+        >
           {newsletters.map(newsletter => (
-            <div className="margin-bottom-m" key={`newsletter-${newsletter.id}`}>
-              <NewsletterTeaser newsletter={newsletter} />
+            <div
+              className="margin-bottom-m"
+              key={`newsletter-${newsletter.id}`}
+            >
+              <NewsletterTeaser
+                newsletter={newsletter}
+              />
             </div>
           ))}
         </div>
 
-        <a className="background-color-yellow-400 border-radius-xxs color-gray-900 inline-block font-size-14-short font-weight-500 padding-horizontal-m padding-vertical-s visited:color-gray-900" href="/newsletter/archive">
+        <a
+          className="background-color-yellow-400 border-radius-xxs color-gray-900 inline-block font-size-14-short font-weight-500 padding-horizontal-m padding-vertical-s visited:color-gray-900"
+          href="/newsletter/archive"
+        >
           Read all newsletters →
         </a>
       </Taper>
