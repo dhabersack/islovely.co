7ae20734ebe4e868dcc4646ecf4c90d7268c696c
Dom Habersack <dom@islovely.co>
2020-12-09 17:51:57 +0100

    Keep more classnames when purging
    This keeps Tailwind’s purge from accidentally removing classes we need,
    but that aren’t present in any of the files it’s looking at.

--- src/components/flash.js
+++ src/components/flash.js
@@ -5,22 +5,22 @@ export default ({
   type,
 }) => {
   const backgroundColor = ({
-    'error': 'red-50',
-    'info': 'blue-50',
-    'success': 'green-50'
-  }[type] || 'gray-50');
+    'error': 'bg-red-50',
+    'info': 'bg-blue-50',
+    'success': 'bg-green-50'
+  }[type] || 'bg-gray-50');
 
   const borderColor = ({
-    'error': 'red-200',
-    'info': 'blue-200',
-    'success': 'green-200'
-  }[type] || 'gray-300');
+    'error': 'border-red-200',
+    'info': 'border-blue-200',
+    'success': 'border-green-200'
+  }[type] || 'border-gray-300');
 
   return (
     <p
       className={`
-        bg-${backgroundColor}
-        border-${borderColor}
+        ${backgroundColor}
+        ${borderColor}
         border
         mb-6
         px-4
