e733a776e21d0c1737946f7d0b77d7803b451053
Dom Habersack <dom@islovely.co>
2021-01-14 13:27:14 +0100

    Remove permalink from fire tip component
    Not even I used that thing.

--- src/components/firetip.js
+++ src/components/firetip.js
@@ -69,36 +69,6 @@ export default ({
           ))}
         </div>
       </div>
-
-      <footer
-        className={`
-          bg-gray-100
-          px-4
-          py-3
-          dark:bg-black
-        `}
-      >
-        <p
-          className={`
-            m-0
-            text-gray-500
-            text-xs
-            dark:text-gray-300
-          `}
-        >
-          <span
-            className="mr-1"
-          >
-            Permalink:
-          </span>
-
-          <a
-            href={`/firetips/${slug}`}
-          >
-            islovely.co/firetips/{slug}
-          </a>
-        </p>
-      </footer>
     </Card>
   )
 }
